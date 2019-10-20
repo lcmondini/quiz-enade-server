ed98925256e463d1d28c89c31795e2590bef372b
Lucas Mondini <lcmondini@gmail.com>
2019-10-20 13:00:21 -0300

    User and Session Controllers and JWT

--- src/app/models/User.js
+++ src/app/models/User.js
@@ -1,4 +1,5 @@
 import Sequelize, { Model } from 'sequelize';
+import bcrypt from 'bcryptjs';
 
 class User extends Model {
   static init(sequelize) {
@@ -7,8 +8,13 @@ class User extends Model {
      */
     super.init(
       {
+        /**
+         * Field the user can send, does not reflect the field on the database
+         * 'Sequelize.VIRTUAL' is a field that does not exists on the database
+         */
         name: Sequelize.STRING,
         email: Sequelize.STRING,
+        password: Sequelize.VIRTUAL,
         password_hash: Sequelize.STRING,
         coordinator: Sequelize.BOOLEAN,
       },
@@ -16,6 +22,21 @@ class User extends Model {
         sequelize,
       }
     );
+
+    /**
+     * Before the user is saved, whether is created or modified, this hook is executed
+     */
+    this.addHook('beforeSave', async user => {
+      if (user.password) {
+        user.password_hash = await bcrypt.hash(user.password, 8);
+      }
+    });
+
+    return this;
+  }
+
+  checkPassword(password) {
+    return bcrypt.compare(password, this.password_hash);
   }
 }
 
---
+++ src/config/auth.js
@@ -0,0 +1,4 @@
+export default {
+  secret: 'd2df79851139f22009989c3bb403cc53',
+  expiresIn: '7d',
+};
