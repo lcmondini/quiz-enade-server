d82f09dd6064b3d4b87db1fa809c78dd9ebaf916
Lucas Mondini <lcmondini@gmail.com>
2019-12-17 01:11:27 -0300

    Add Image Field to Questions

--- src/app/models/Question.js
+++ src/app/models/Question.js
@@ -21,6 +21,10 @@ class Question extends Model {
 
     return this;
   }
+
+  static associate(models) {
+    this.belongsTo(models.File, { foreignKey: 'image_id', as: 'image' });
+  }
 }
 
 export default Question;
